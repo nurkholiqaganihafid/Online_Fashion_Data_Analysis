41e0e1492595aa49705a8b7b800f76e1cb4e308d
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2023-10-24 22:23:13 +0700

    problem solved

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -63,7 +63,7 @@ def create_rfm_df(df):
     
     return rfm_df
 
-all_df = pd.read_csv('/dashboard/all_data.csv')
+all_df = pd.read_csv('https://raw.githubusercontent.com/nurkholiqaganihafid/Online_Fashion_Data_Analysis/main/dashboard/all_data.csv')
 
 datetime_columns = ['order_date', 'delivery_date']
 all_df.sort_values(by='order_date', inplace=True)
