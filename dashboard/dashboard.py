fa2a2e3c88c88bc5c0b98383045f81086b0615db
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2023-10-24 22:11:54 +0700

    modified dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -63,7 +63,7 @@ def create_rfm_df(df):
     
     return rfm_df
 
-all_df = pd.read_csv('all_data.csv')
+all_df = pd.read_csv('/dashboard/all_data.csv')
 
 datetime_columns = ['order_date', 'delivery_date']
 all_df.sort_values(by='order_date', inplace=True)
